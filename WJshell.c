8cfe259e46ff8cb58861afd0e9342631c8c08fb0
stwjivan <stwj.ivan@gmail.com>
2016-02-28 17:38:26 -0500

    implement built-in command cd, and fix bug on crashing with no command input

--- WJshell.c
+++ WJshell.c
@@ -6,14 +6,18 @@
 #include <pwd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <errno.h>
+#include <limits.h>
 
 #include "cmd_structure.h"
 #include "command.h"
 
 void print_prompt () {
 	struct passwd *user;
+	char buffer[PATH_MAX+1];
+	char *cwd=getcwd(buffer,PATH_MAX+1);
 	user=getpwuid(getuid());
-	printf("%s@WJShell:$ ",user->pw_name);
+	printf("%s@WJShell:%s$ ",user->pw_name,cwd);
 }
 
 int main () {
@@ -21,14 +25,21 @@ int main () {
 	while (1) {
 		print_prompt();
 		fgets(s,sizeof s, stdin);
+		if (strcmp(s,"\n")==0) {
+			continue; //if there is no input,skip reading command
+		}
 		struct Command command;
 		ReadCommand(s,&command);
 		ReadRedirectAndBackground(&command);
-		//struct SubCommand *last=&(command.sub_commands[command.num_sub_commands-1]); 
 		int i;
 		int fds[2];
 		int pre_pipein=0;
 		for (i=0;i<command.num_sub_commands;i++) {
+			if (strcmp(command.sub_commands[i].argv[0],"cd")==0) {
+				int err=chdir(command.sub_commands[i].argv[1]);
+				if (err <0) perror("cd");
+				break;
+			}
 			if(i<command.num_sub_commands-1) {
 				int err=pipe(fds);
 				if (err<0) perror("pipe");
@@ -37,34 +48,50 @@ int main () {
 			if (pid==0) {
 				//child process
 				dup2(pre_pipein,0); //change the stdin to accept data from previous process
-				if (i==0 && command.stdin_redirect!=NULL) {
+				if (i==0 && command.stdin_redirect!=NULL) { 
+					//implement redirect-in, only the first command need to redirect
 					close(0);
 					int fd=open(command.stdin_redirect,O_RDONLY);
 					if (fd<0) {
-						perror(command.stdin_redirect);
-						exit(1);	
+						if (errno==ENOENT) {
+							fprintf(stderr, "%s : File not found\n", command.stdin_redirect);
+							exit(1);
+						} else {
+							perror(command.stdin_redirect);
+							exit(1);
+						}	
 					}
 				}
-				if (i==command.num_sub_commands-1 && command.stdout_redirect!=NULL) {
+				if (i==command.num_sub_commands-1 && command.stdout_redirect!=NULL) { 
+					//implement redirect-out, only the last subcommand need to redirect
 					close(1);
 					int fd=open(command.stdout_redirect, O_WRONLY|O_CREAT,0660);
 					if (fd<0) {
-						perror(command.stdout_redirect);
-						exit(1);
+						if (errno==ENOENT) {
+							fprintf(stderr, "%s : Cannot create file\n", command.stdin_redirect);
+							exit(1);
+						} else {
+							perror(command.stdout_redirect);
+							exit(1);
+						}
 					}
 				}
-
 				if (i!=command.num_sub_commands-1) {
 					dup2(fds[1],1);
 				}
 				close(fds[0]);
 				if(execvp(command.sub_commands[i].argv[0],command.sub_commands[i].argv)<0) {
-					perror(command.sub_commands[i].argv[0]);
+					if (errno==ENOENT) {
+						fprintf(stderr, "%s : Command not found\n", command.sub_commands[i].argv[0]);
+					} else {
+						perror(command.sub_commands[i].argv[0]);
+					}
 				}
 			} else {
 				//parent process
 				//close the unused pipe port
 				close(fds[1]);
+				//save the pipe input for next fork
 				pre_pipein=fds[0];
 				//print the pid if running in the background
 				if(command.background==1 && i==command.num_sub_commands-1) {
